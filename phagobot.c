21140c71f4f9a0e78e4cf6d37253b332d5487f2f
Joe MacInnes <joemacinnes@Joes-MacBook-Air.local>
2018-04-29 20:56:15 -0400

    Add documentation and adjust Makefile target

--- phagobot.c
+++ phagobot.c
@@ -1,6 +1,23 @@
-/* Kilobot Edge following demo
- * 
- * Ivica Slavkov, Fredrik Jansson  2015
+/*
+ * Joe MacInnes
+ * April 29, 2018
+ *
+ * This program simulates the process of phagocytosis.
+ * It borrows some code from the follow_edge and gradient
+ * examples found at
+ * https://github.com/JIC-CSB/kilombo
+ *
+ * It is designed to run on the Kilombo simulator.
+ *
+ * To run the program requires installing the Kilombo
+ * simulator from the above link and compiling the code
+ * using the provided Makefile
+ *
+ * Once the executable is built
+ * './phagobot -b bot_pos.json'
+ * Will run the program using the simulator parameters
+ * and starting bot positions found in kilombo.json
+ * and bot_pos.json
  */
 
 #include <math.h>
@@ -12,29 +29,24 @@ enum {STOP,LEFT,RIGHT,STRAIGHT};
 
 typedef struct
 {
-  Neighbor_t neighbors[MAXN];
-  int N_Neighbors;
+  Neighbor_t neighbors[MAXN]; // list of neighbor information
+  int N_Neighbors; // number of neighbors
   uint8_t bot_type;
   uint8_t bot_state;
   uint8_t move_type;
-  uint32_t inertia_stamp;
-  int stall_counter;
-  int own_gradient;
-  uint16_t my_reading;
-  uint32_t momentum_check;
-  bool lost;
-  int max;
-  uint16_t dummy;
-  message_t transmit_msg;
-  char message_lock;
-  received_message_t RXBuffer[RB_SIZE];
-  uint8_t RXHead, RXTail;
-  //bool adhesion;
-  bool lock;
-  uint16_t lowest_reading;
-} MyUserdata;
+  uint8_t RXHead, RXTail;  // used for simulated message processing
+  uint16_t my_reading; // own light reading
+  uint32_t momentum_check; // time kilobot has been alone
+  int own_gradient; // hops from adhesion site kilobot
+  int min; // index of neighbor with highest light reading
+  bool lock; // flag for adhesion kilobot movement
+  bool lost; // flag for no neighbors
+  char message_lock; // prevents sending of message
+  message_t transmit_msg; // message data object
+  received_message_t RXBuffer[RB_SIZE]; // usde for simulated message processing
+} bot_info;
 
-REGISTER_USERDATA(MyUserdata)
+REGISTER_USERDATA(bot_info)
 
 #ifdef SIMULATOR
 #include <stdio.h>    // for printf
@@ -42,20 +54,6 @@ REGISTER_USERDATA(MyUserdata)
 #define DEBUG         // for printf to serial port
 #include "debug.h"
 #endif
-
-uint8_t colorNum[] = {
-  RGB(0,0,0),  //0 - off
-  RGB(1,0,0),  //1 - red
-  RGB(0,1,0),  //2 - green
-  RGB(0,0,1),  //3 - blue
-  RGB(1,1,0),  //4 - yellow
-  RGB(0,1,1),  //5 - cyan
-  RGB(1,0,1),  //6 - purple
-  RGB(2,1,0),  //7  - orange
-  RGB(1,1,1),  //8  - white
-  RGB(3,3,3)   //9  - bright white
-};
-
 
 // message rx callback function. Pushes message to ring buffer.
 void rxbuffer_push(message_t *msg, distance_measurement_t *dist) {
@@ -72,6 +70,8 @@ message_t *message_tx()
   return &mydata->transmit_msg;
 }
 
+/* Accessor and mutator functions for bot state and move type
+ */
 void set_bot_state(int state)
 {
   mydata->bot_state = state;
@@ -96,7 +96,7 @@ int get_move_type(void)
  * Go through the list of neighbors. If the message is from a bot
  * already in the list, update the information, otherwise
  * add a new entry in the list
- */
+*/
 
 void process_message()
 {
@@ -134,40 +134,49 @@ void process_message()
   mydata->neighbors[i].grad = data[6];
 }
 
-int max_reading(){
-    int index = -1;
-    uint16_t max = mydata->my_reading;
-    uint16_t max_ttl = 0;
-    for (int i = 0; i < mydata->N_Neighbors; i++){
-        if (((mydata->neighbors[i].reading < max) && mydata->neighbors[i].ttl > 0) || ((mydata->neighbors[i].reading <= max) && (mydata->neighbors[i].ttl > max_ttl)))
-        {
-            max_ttl = mydata->neighbors[i].ttl;
-            max = mydata->neighbors[i].reading;
-            index = i;
-        }
+
+/* Finds the minimum light reading by comparing all
+ * neighbors' readings to own. Returns the index of neighbor
+ * with lowest light reading and highest time to live of
+ * reading or -1 if this kilobot has lowest light
+ */
+int min_reading(){
+  int index = -1;
+  uint16_t min = mydata->my_reading;
+  uint16_t max_ttl = 0;
+  for (int i = 0; i < mydata->N_Neighbors; i++){
+    if (((mydata->neighbors[i].reading < min) && mydata->neighbors[i].ttl > 0) || ((mydata->neighbors[i].reading <= min) && (mydata->neighbors[i].ttl > max_ttl)))
+    {
+      max_ttl = mydata->neighbors[i].ttl;
+      min = mydata->neighbors[i].reading;
+      index = i;
     }
-    if (max == mydata->my_reading){
-        return -1;
-    }
-    
-    return index;
+  }
+  if (min == mydata->my_reading){
+    return -1;
+  }
+  return index;
 }
 
+/* Sets the gradient value of the kilobot by searching
+ * through all neighbors finding the lowest gradient
+ * and setting own_gradient to that value + 1.
+ * Forces gradient to be 0 if the kilobot's own light
+ * reading is below a hardcoded threshold (in this case 70)
+ */
 void set_gradient(){
-    int min_grad = mydata->own_gradient;
-    if (mydata->my_reading < 70){
-        mydata->own_gradient = 0;
+  int min_grad = mydata->own_gradient;
+  if (mydata->my_reading < 70)
+    mydata->own_gradient = 0;
+  else if (mydata->min != -1){
+    for (int i = 0; i < mydata->N_Neighbors; i++){
+      if (mydata->neighbors[i].grad < min_grad)
+        min_grad = mydata->neighbors[i].grad;
     }
-    else if (mydata->max != -1){
-        for (int i = 0; i < mydata->N_Neighbors; i++){
-            if (mydata->neighbors[i].grad < min_grad)
-                min_grad = mydata->neighbors[i].grad;
-        }
-        mydata->own_gradient = min_grad + 1;
-    }
-    else{
-        mydata->own_gradient = 0;
-    }
+    mydata->own_gradient = min_grad + 1;
+  }
+  else
+    mydata->own_gradient = 0;
 }
 
 /* Go through the list of neighbors, remove entries older than a threshold,
@@ -194,19 +203,25 @@ void setup_message(void)
   mydata->transmit_msg.data[1] = kilo_uid >> 8;       // 1 high ID
   mydata->transmit_msg.data[2] = mydata->N_Neighbors; // 2 number of neighbors
   mydata->transmit_msg.data[3] = get_bot_state();     // 3 bot state
-  mydata->my_reading = get_ambientlight();
-  mydata->max = max_reading();
-  if (mydata->max != -1){
-    mydata->transmit_msg.data[4] = mydata->neighbors[mydata->max].reading >> 8;
-    mydata->transmit_msg.data[5] = 0 |  mydata->neighbors[mydata->max].reading;
-    mydata->transmit_msg.data[7] = mydata->neighbors[mydata->max].ttl;
+  mydata->my_reading = get_ambientlight();            // record current light
+  mydata->min = min_reading();
+  if (mydata->min != -1){ // broadcast own light reading with full TTL
+    mydata->transmit_msg.data[4] = mydata->neighbors[mydata->min].reading >> 8;
+    // 4 low light reading
+    mydata->transmit_msg.data[5] = 0 |  mydata->neighbors[mydata->min].reading;
+    // 5 high light reading
+    mydata->transmit_msg.data[7] = mydata->neighbors[mydata->min].ttl;
+    // 7 time to live
     }
-  else{
+  else{ // broadcast neighbor's light reading with their TTL
     mydata->transmit_msg.data[4] = mydata->my_reading >> 8;
+    // 4 low light reading
     mydata->transmit_msg.data[5] = 0 | mydata->my_reading;
+    // 5 high light reading
     mydata->transmit_msg.data[7] = TTL;
+    // 7 time to live
     }
-  mydata->transmit_msg.data[6] = mydata->own_gradient;
+  mydata->transmit_msg.data[6] = mydata->own_gradient; // 6 gradient value
   mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);
   mydata->message_lock = 0;
 }
@@ -214,13 +229,10 @@ void setup_message(void)
 void setup()
 {
   rand_seed(kilo_uid + 1); //seed the random number generator
-  mydata->dummy = rand_soft();
   mydata->message_lock = 0;
-  mydata->lowest_reading = 2000;
   mydata->own_gradient = 0;
-  mydata->max = 0;
+  mydata->min = 0;
   mydata->lost = false;
-  mydata->stall_counter = 0;
   mydata->my_reading = get_ambientlight();
   mydata->N_Neighbors = 0;
   set_move_type(STOP);
@@ -231,90 +243,84 @@ void setup()
 void receive_inputs()
 {
   while (!RB_empty())
-    {
-      process_message();
-      RB_popfront();
-    }
+  {
+    process_message();
+    RB_popfront();
+  }
   purgeNeighbors();
 }
 
-uint8_t get_dist_by_ID(uint16_t bot)
-{
-  uint8_t i;
-  uint8_t dist = 255;
-  
-  for(i = 0; i < mydata->N_Neighbors; i++)
-    {
-      if(mydata->neighbors[i].ID == bot)
-	{
-	  dist = mydata->neighbors[i].dist;
-	  break;
-	}
-    }
-  return dist;
-}
-
+/*
+ * Returns the distance of the nearest neighbor
+ */
 uint8_t find_nearest_N_dist()
 {
   uint8_t i;
   uint8_t dist = 90;
 
   for(i = 0; i < mydata->N_Neighbors; i++)
+  {
+    if(mydata->neighbors[i].dist < dist)
     {
-      if(mydata->neighbors[i].dist < dist)
-	{
-	  dist = mydata->neighbors[i].dist;
-	}
+      dist = mydata->neighbors[i].dist;
     }
+  }
   return dist;
 }
 
+/*
+ * Returns the distance of the nearest neighbor
+ */
 uint8_t find_farthest_N_dist()
 {
-    uint8_t i;
-    uint8_t dist = 0;
-    for(i = 0; i < mydata->N_Neighbors; i++)
+  uint8_t i;
+  uint8_t dist = 0;
+  for(i = 0; i < mydata->N_Neighbors; i++)
+  {
+    if(mydata->neighbors[i].dist > dist)
     {
-        if(mydata->neighbors[i].dist > dist)
-        {
-            dist = mydata->neighbors[i].dist;
-        }
+      dist = mydata->neighbors[i].dist;
     }
-    return dist;
+  }
+  return dist;
 }
 
-
-
+/*
+ * Follows the edge of the 'cell' of kilobots.
+ * If too close to any neighbor, the kilobot turns one direction
+ * if far enough away, the kilobot turns the opposite direction
+ */
 void follow_edge()
 {
   uint8_t desired_dist = 42;
   set_bot_state(MOVE);
+  // currently using modulo 2 so that half the kilobots
+  // the edge in one direction and the other half in the
+  // other direction. This NEEDS to change, very sloppy code
   if(kilo_uid % 2 == 1){
   if(find_nearest_N_dist() > desired_dist)
-    {
-      if(get_move_type() == LEFT)
+  {
+    if(get_move_type() == LEFT)
       spinup_motors();
-      set_motors(0, kilo_turn_right);
-      set_move_type(RIGHT);
-    }
-
+    set_motors(0, kilo_turn_right);
+    set_move_type(RIGHT);
+  }
   else
-    {
-      if(get_move_type() == RIGHT)
+  {
+    if(get_move_type() == RIGHT)
       spinup_motors();
-      set_motors(kilo_turn_left, 0);
-      set_move_type(LEFT);
+    set_motors(kilo_turn_left, 0);
+    set_move_type(LEFT);
     }
   }
   else{
     if(find_nearest_N_dist() > desired_dist)
     {
       if(get_move_type() == RIGHT)
-        spinup_motors();
+         spinup_motors();
       set_motors(kilo_turn_left, 0);
       set_move_type(LEFT);
     }
-    
     else
     {
       if(get_move_type() == LEFT)
@@ -322,152 +328,147 @@ void follow_edge()
       set_motors(0, kilo_turn_right);
       set_move_type(RIGHT);
     }
-    
   }
 }
 
 void loop()
 {
-    
   //receive messages
   receive_inputs();
-    set_gradient();
-    if (mydata->my_reading < 70){
-        spinup_motors();
-        set_motors(0, 0);
-        set_move_type(STOP);
-        set_bot_state(LISTEN);
-         set_color(RGB(0,0,1));
-        mydata->own_gradient = 0;
-        
-    }
-    else if (mydata->own_gradient != 0){
-      //mydata->adhesion = false;
-      bool highest = true;
-      bool only_mover = true;
-        mydata->lock = false;
-        for (int i = 0; i < mydata->N_Neighbors; i++){
-            if (mydata->neighbors[i].n_bot_state == MOVE){
-                only_mover = false;
-            }
-        }
-        for (int i = 0; i < mydata->N_Neighbors; i++){
-            if (mydata->neighbors[i].grad > mydata->own_gradient){
-                highest = false;
-            }
-        }
-      if(highest && only_mover)
-        {
-          set_color(RGB(1,0,0));
-          follow_edge();
-        }
-      else{
-          set_color(RGB(0,1,0));
-          set_bot_state(LISTEN);
-          spinup_motors();
-          set_motors(0, 0);
-          set_move_type(STOP);
+  set_gradient();
+  // within stopping threshold
+  if (mydata->my_reading < 70){
+    spinup_motors();
+    set_motors(0, 0);
+    set_move_type(STOP);
+    set_bot_state(LISTEN);
+    set_color(RGB(0,0,1));
+    mydata->own_gradient = 0;
+  }
+  // kilobot is NOT an adhesion site
+  else if (mydata->own_gradient != 0){
+    bool highest = true;
+    bool only_mover = true;
+    mydata->lock = false;
+    // check to see if any neighbors are moving and if have
+    // highest gradient
+    for (int i = 0; i < mydata->N_Neighbors; i++){
+      if (mydata->neighbors[i].n_bot_state == MOVE){
+        only_mover = false;
+      }
+      if (mydata->neighbors[i].grad > mydata->own_gradient){
+        highest = false;
       }
     }
-    else if (mydata->N_Neighbors == 0){
-        //it appears that 14seconds is around how long it takes for the kilobot to turn 180 degrees
-        if (!mydata->lost){
-            mydata->momentum_check = kilo_ticks;
-        }
-        mydata->lost = true;
-        
-        if (kilo_ticks - mydata->momentum_check > 515){
-            spinup_motors();
-            set_motors(kilo_turn_left, kilo_turn_right);
-            set_move_type(STRAIGHT);
-        }
-        else
-            set_color(RGB(1,1,1));
+    // Follow the edge if both no neighbors are moving and have
+    // highest gradient
+    if(highest && only_mover)
+    {
+      set_color(RGB(1,0,0));
+      follow_edge();
     }
-    else {
-        //adhesion site
-        if (find_nearest_N_dist() > 55){
-            spinup_motors();
-            set_motors(0, 0);
-            set_move_type(STOP);
-            
-            mydata->lock = true;
-        }
-        else if (!mydata->lock && mydata->N_Neighbors > 0){
-            spinup_motors();
-            set_motors(kilo_turn_left, kilo_turn_right);
-            set_move_type(STRAIGHT);
-        }
-        set_color(RGB(0,0,1));
-        
-        
-        set_bot_state(LISTEN);
+    // Stay stationary
+    else{
+      set_color(RGB(0,1,0));
+      set_bot_state(LISTEN);
+      spinup_motors();
+      set_motors(0, 0);
+      set_move_type(STOP);
     }
-    
-    
-    
-  
-  setup_message();
+  }
+  // If the kilobot is all alone
+  else if (mydata->N_Neighbors == 0){
+    // it appears that 14seconds is around how long it
+    // takes for the kilobot to turn 180 degrees
+    if (!mydata->lost){
+        mydata->momentum_check = kilo_ticks;
+    }
+    mydata->lost = true;
+    if (kilo_ticks - mydata->momentum_check > 515){
+      spinup_motors();
+      set_motors(kilo_turn_left, kilo_turn_right);
+      set_move_type(STRAIGHT);
+    }
+    else
+      set_color(RGB(1,1,1));
+  }
+  // Otherwise, the kilobot is an adhesion site
+  else {
+    // If far enough away from other kilobots stop moving
+    if (find_nearest_N_dist() > 55){
+      spinup_motors();
+      set_motors(0, 0);
+      set_move_type(STOP);
+      mydata->lock = true;
+    }
+    // Otherwise move forward
+    else if (!mydata->lock && mydata->N_Neighbors > 0){
+      spinup_motors();
+      set_motors(kilo_turn_left, kilo_turn_right);
+      set_move_type(STRAIGHT);
+    }
+    set_color(RGB(0,0,1));
+    set_bot_state(LISTEN);
+  }
+  setup_message(); // prepare the next message
 }
-
+/*
+ * Beyond this point is all helper code for the simulator
+ */
 extern char* (*callback_botinfo) (void);
 char *botinfo(void);
 int16_t callback_lighting(double, double);
 int main(void)
 {
   kilo_init();
-
 #ifdef DEBUG
   // setup debugging, i.e. printf to serial port, in real Kilobot
   debug_init();
 #endif
-  
   SET_CALLBACK(botinfo, botinfo);
   SET_CALLBACK(reset, setup);
   SET_CALLBACK(lighting, callback_lighting);
   RB_init();                       // initialize ring buffer
   kilo_message_rx = rxbuffer_push;
   kilo_message_tx = message_tx;    // register our transmission function
-
   kilo_start(setup, loop);
-
   return 0;
 }
-
 #ifdef SIMULATOR
 // provide a text string for the status bar, about this bot
 static char botinfo_buffer[10000];
+//print out information about the bot
 char *botinfo(void)
 {
   int n;
   char *p = botinfo_buffer;
   n = sprintf (p, "ID: %d ", kilo_uid);
-    p += n;
+  p += n;
   n = sprintf (p, "my reading: %i ", mydata->my_reading);
   p += n;
-    n = sprintf (p, "light: %d ", get_ambientlight());
-    p += n;
-    n = sprintf (p, "transmitted reading: %i ", (mydata->transmit_msg.data[4] << 8) | mydata->transmit_msg.data[5]);
-    p += n;
-    n = sprintf (p, " ttl: %i ", (mydata->transmit_msg.data[7]));
-    p += n;
-    n = sprintf (p, "gradient: %i ", mydata->own_gradient);
-    p += n;
-
-  n = sprintf (p, "Ns: %d, dist: %d\n ", mydata->N_Neighbors, find_nearest_N_dist());
+  n = sprintf (p, "light: %d ", get_ambientlight());
+  p += n;
+  n = sprintf (p, "transmitted reading: %i ", (mydata->transmit_msg.data[4] << 8) | mydata->transmit_msg.data[5]);
+  p += n;
+  n = sprintf (p, " transmitted ttl: %i ", (mydata->transmit_msg.data[7]));
+  p += n;
+  n = sprintf (p, "gradient: %i ", mydata->own_gradient);
   p += n;
 
   return botinfo_buffer;
 }
 
-// Define lighting simulation
-
+/*
+ * Uses euclidean distance to setup a lighting callback function
+ * Lower values returned values correspond to higher light
+ * Light readings degrade linearly
+ */
 int16_t callback_lighting(double x, double y){
-    double light_y = 0;
-    double light_x = -150;
-    double dist_x = pow(light_x + x, 2);
-    double dist_y = pow(light_y + y, 2);
-    double dist_c = sqrt(dist_x + dist_y);
-    return (int16_t)dist_c;
+  double light_y = 0;
+  double light_x = -150;
+  double dist_x = pow(light_x + x, 2);
+  double dist_y = pow(light_y + y, 2);
+  double dist_c = sqrt(dist_x + dist_y);
+  return (int16_t)dist_c;
 }
 #endif
--- phagobot.h
+++ phagobot.h
@@ -1,45 +1,43 @@
+/*
+ * Joe MacInnes
+ * April 29, 2018
+ *
+ * This file contains the neighbor data
+ * type definition as well as helper macro definitions
+ * for the Kilombo simulator
+ */
+
 #ifndef GRN_H
 #define GRN_H
-
 #include <math.h>
-
 #ifndef M_PI
 #define M_PI 3.141592653589793238462643383279502884197169399375105820974944
 #endif
-
 #define GENES 6
 #define DIFFGENES 6  // Maximal number of diffusing genes, limited by the message length.
-
-
 #define RB_SIZE 16  // Ring buffer size. Choose a power of two for faster code
                     // memory usage: 16*RB_SIZE
                     // 8 works too, but complains in the simulator
                     // when the bots are very dense
-
-#define MAXN 20
-#define TTL 100
+#define MAXN 20 // Max number of neighbors
+#define TTL 100 // max times an original light reading can transmit
 extern uint8_t NGenes;
-
 enum BOTTYPE {LAST, FOLLOWER, LEADER};
 enum BOTSTATES {WAIT, LISTEN, MOVE};
-
 typedef struct {
-  uint16_t ID;
-  uint8_t dist;
+  uint8_t dist; // distance to neighbor
   uint8_t n_bot_state;
-  uint8_t N_Neighbors;
-  uint32_t timestamp;
-  uint16_t ttl;
-  uint8_t grad;
-  uint16_t reading;
+  uint8_t N_Neighbors; // number of neighbors
+  uint8_t grad; // gradient of neighbor
+  uint16_t ID; // kilo_uid of neighbor
+  uint16_t ttl; // time to live of light  reading sent by neighbor
+  uint16_t reading; // light reading sent by neighbor
+  uint32_t timestamp; // last time heard from neighbor
 } Neighbor_t;
-
 typedef struct {
     message_t msg;
     distance_measurement_t dist;
 } received_message_t;
-
-
 
 // Ring buffer operations. Taken from kilolib's ringbuffer.h
 // but adapted for use with mydata->
@@ -73,32 +71,6 @@ typedef struct {
       { mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;	\
 	printf("Full.\n"); }				\
   }
-
-
-/*
-// Ring buffer operations indexed with head, count
-// These save one entry in the buffer, but are less interrupt safe due to the shared count
-
-#define RB_init() {	\
-    mydata->RXHead = 0; \
-    mydata->RXCount = 0;\
-}
-#define RB_empty() (mydata->RXCount == 0)
-#define RB_full()  (mydata->RXCount == RB_SIZE)
-#define RB_front() (mydata->RXBuffer[mydata->RXHead])
-#define RB_back()  (mydata->RXBuffer[(mydata->RXHead+mydata->RXCount)%RB_SIZE])
-#define RB_popfront() {\
-    mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;\
-    mydata->RXCount--;\
-}
-
-#define RB_pushback() {\
-    if (RB_full())\
-        mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;\
-    else\
-      mydata->RXCount++;\
-}
-*/
 
 
 #endif
